c0d9c0d6feaa5cf3777d7a3fba7c45b51e6dfd2a
iMinusMinus <mean.leung@outlook.com>
2023-04-21 14:59:55 +0800

    chromium

--- replit.nix
+++ replit.nix
@@ -2,6 +2,9 @@
     deps = [
         pkgs.graalvm17-ce
         pkgs.maven
+        pkgs.chromedriver
+        pkgs.chromium
+#        pkgs.google-chrome
         pkgs.replitPackages.jdt-language-server
         pkgs.replitPackages.java-debug
     ];
